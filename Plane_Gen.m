0150d80438e609ef1ab622c23081a766404bc9a2
roshenam <roshenam@stanford.edu>
2016-06-22 16:45:19 -0700

    Code now generates all 8 planes and their normal vectors for use as constraints

--- Plane_Gen.m
+++ Plane_Gen.m
@@ -1,7 +1,6 @@
-% Generates the points on the spherical target that the cosntraint plane
-% must intersect with
+%% Generates points on the target to intersect with constraint planes
 
-theta = pi/4; phi = -pi/4; rho = 5*pi/180;
+theta = -pi/4; phi = -pi/4; rho = 10*pi/180;
 rp = 0.2; % [m] radius of target
 R1 = [cos(theta) sin(theta) 0; -sin(theta) cos(theta) 0; 0 0 1];
 R2 = [cos(phi) 0 -sin(phi); 0 1 0; sin(phi) 0 cos(phi)];
@@ -12,14 +11,6 @@ Rinv = inv(R);
 rvec = Rinv*[rp; 0; 0];
 Xt = X.*rp; Yt = Y.*rp; Zt = Z.*rp;
 Xp = X.*rp/40; Yp = Y.*rp/40; Zp = Z.*rp/40;
-surf(Xt,Yt,Zt,'Facecolor', [0 1 0])
-hold all
-surf(Xp+rvec(1), Yp+rvec(2), Zp+rvec(3),'Facecolor',[1 0 0])
-xlabel('X axis')
-ylabel('Y axis')
-zlabel('Z axis')
-
-view([120 20])
 
 % Rotations to generate points at NSWE corners of circle
 Ry = [cos(rho) 0 -sin(rho); 0 1 0; sin(rho) 0 cos(rho)];
@@ -42,22 +33,50 @@ Rp3b = Rn*Rx*R; Rp3binv = inv(Rp3b);
 Rp4a = Rs*Rxn*R; Rp4ainv = inv(Rp4a);
 Rp4b = Rn*Rxn*R; Rp4binv = inv(Rp4b);
 
-p1avec = Rp1ainv*[rp; 0; 0];
-p1bvec = Rp1binv*[rp; 0; 0];
-p2avec = Rp2ainv*[rp; 0; 0];
-p2bvec = Rp2binv*[rp; 0; 0];
-p3avec = Rp3ainv*[rp; 0; 0];
-p3bvec = Rp3binv*[rp; 0; 0];
-p4avec = Rp4ainv*[rp; 0; 0];
-p4bvec = Rp4binv*[rp; 0; 0];
+pS = Rp1ainv*[rp; 0; 0];
+pN = Rp1binv*[rp; 0; 0];
+pE = Rp2ainv*[rp; 0; 0];
+pW = Rp2binv*[rp; 0; 0];
+pSE = Rp3ainv*[rp; 0; 0];
+pNW = Rp3binv*[rp; 0; 0];
+pSW = Rp4ainv*[rp; 0; 0];
+pNE = Rp4binv*[rp; 0; 0];
 
-surf(Xp+p1avec(1), Yp+p1avec(2), Zp+p1avec(3),'Facecolor',[1 0 0])
-surf(Xp+p1bvec(1), Yp+p1bvec(2), Zp+p1bvec(3),'Facecolor',[1 0 0])
-surf(Xp+p2avec(1), Yp+p2avec(2), Zp+p2avec(3),'Facecolor',[1 0 0])
-surf(Xp+p2bvec(1), Yp+p2bvec(2), Zp+p2bvec(3),'Facecolor',[1 0 0])
-surf(Xp+p3avec(1), Yp+p3avec(2), Zp+p3avec(3),'Facecolor',[1 0 0])
-surf(Xp+p3bvec(1), Yp+p3bvec(2), Zp+p3bvec(3),'Facecolor',[1 0 0])
-surf(Xp+p4avec(1), Yp+p4avec(2), Zp+p4avec(3),'Facecolor',[1 0 0])
-surf(Xp+p4bvec(1), Yp+p4bvec(2), Zp+p4bvec(3),'Facecolor',[1 0 0])
+%% Plot points to make sure the code is making sense
+target = surf(Xt,Yt,Zt,'Facecolor', [0 1 0]);
+hold all
+surf(Xp+rvec(1), Yp+rvec(2), Zp+rvec(3),'Facecolor',[1 0 0])
+% surf(Xp+pS(1), Yp+pS(2), Zp+pS(3),'Facecolor',[1 0 0])
+% surf(Xp+pN(1), Yp+pN(2), Zp+pN(3),'Facecolor',[1 0 0])
+% surf(Xp+pE(1), Yp+pE(2), Zp+pE(3),'Facecolor',[1 0 0])
+% surf(Xp+pW(1), Yp+pW(2), Zp+pW(3),'Facecolor',[1 0 0])
+% surf(Xp+pSE(1), Yp+pSE(2), Zp+pSE(3),'Facecolor',[1 0 0])
+% surf(Xp+pNW(1), Yp+pNW(2), Zp+pNW(3),'Facecolor',[1 0 0])
+% surf(Xp+pSW(1), Yp+pSW(2), Zp+pSW(3),'Facecolor',[1 0 0])
+% surf(Xp+pNE(1), Yp+pNE(2), Zp+pNE(3),'Facecolor',[1 0 0])
 
+xlabel('X axis')
+ylabel('Y axis')
+zlabel('Z axis')
+
+view([90+theta*180/(2*pi) -phi*180/(2*pi)])
+
+%% Plot 8 planes to approximate the cone
+X = [0 0 0 0 0 0 0 0; 2*pN(1) 2*pNE(1) 2*pE(1) 2*pSE(1) 2*pS(1) 2*pSW(1) 2*pW(1) 2*pNW(1);
+    2*pNE(1) 2*pE(1) 2*pSE(1) 2*pS(1) 2*pSW(1) 2*pW(1) 2*pNW(1) 2*pN(1)];
+Y = [0 0 0 0 0 0 0 0; 2*pN(2) 2*pNE(2) 2*pE(2) 2*pSE(2) 2*pS(2) 2*pSW(2) 2*pW(2) 2*pNW(2);
+    2*pNE(2) 2*pE(2) 2*pSE(2) 2*pS(2) 2*pSW(2) 2*pW(2) 2*pNW(2) 2*pN(2)];
+Z = [0 0 0 0 0 0 0 0; 2*pN(3) 2*pNE(3) 2*pE(3) 2*pSE(3) 2*pS(3) 2*pSW(3) 2*pW(3) 2*pNW(3);
+    2*pNE(3) 2*pE(3) 2*pSE(3) 2*pS(3) 2*pSW(3) 2*pW(3) 2*pNW(3) 2*pN(3)];
+C = [0.5 0.5 0.5 0.5 0.5 0.5 0.5 0.5;
+     1 1 1 1 1 1 1 1;
+     0.3 0.3 0.3 0.3 0.3 0.3 0.3 0.3];
+h = fill3(X,Y,Z,C);
+alpha(h, .5)
+
+%% Compute normal vectors for each plane
+p1 = cross(pN, pNE); p2 = cross(pNE, pE); 
+p3 = cross(pE, pSE); p4 = cross(pSE, pS);
+p5 = cross(pS, pSW); p6 = cross(pSW, pW); 
+p7 = cross(pW, pNW); p8 = cross(pNW, pN);
 
