f084435998594b3e4aa306e1aa4d93c4f8342ed7
roshenam <roshenam@stanford.edu>
2016-06-27 11:27:11 -0700

    Changed to paper values instead of my calculated values

--- make_C.m
+++ make_C.m
@@ -9,9 +9,17 @@ L4 = cos(phik-gamma)/((rp-rtol)*sin(gamma));
 
 % Different than the paper. This is what I got from my math. Check later
 % with their math
-C(1,1) = L1 - phik*L2;
-C(1,2) = -(L2 + phik*L1);
-C(1,10) = L1*yk + L2*xk;
-C(2,1) = -L3 - phik*L2;
-C(2,2) = L4 - phik*L3;
-C(2,10) = -L3*yk - L4*xk;
+% C(1,1) = L1 - phik*L2;
+% C(1,2) = -(L2 + phik*L1);
+% C(1,10) = L1*yk + L2*xk;
+% C(2,1) = -L3 - phik*L2;
+% C(2,2) = L4 - phik*L3;
+% C(2,10) = -L3*yk - L4*xk;
+
+C(1,1) = sin(phik+gamma)/((rp-rtol)*sin(gamma));
+C(1,2) = -cos(phik+gamma)/((rp-rtol)*sin(gamma));
+C(2,1) = -sin(phik-gamma)/((rp-rtol)*sin(gamma));
+C(2,2) = cos(phik-gamma)/((rp-rtol)*sin(gamma));
+C(1,10) = L2*xk+L1*yk;
+C(2,10) = -L4*xk-L3*yk;
+
