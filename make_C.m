c68a1a86b46ca6b959aa9bf9dc71649cabbf6e20
roshenam <roshenam@stanford.edu>
2016-06-27 16:18:40 -0700

    Updated to paper version

--- make_C.m
+++ make_C.m
@@ -1,7 +1,7 @@
 function C = make_C(params, xk, yk, phik)
 
 rp = params.rp; rtol = params.rtol; gamma = params.gamma;
-C = zeros(2,10);
+C = zeros(2,6);
 L1 = sin(phik+gamma)/((rp-rtol)*sin(gamma));
 L2 = cos(phik+gamma)/((rp-rtol)*sin(gamma));
 L3 = sin(phik-gamma)/((rp-rtol)*sin(gamma));
@@ -16,10 +16,10 @@ L4 = cos(phik-gamma)/((rp-rtol)*sin(gamma));
 % C(2,2) = L4 - phik*L3;
 % C(2,10) = -L3*yk - L4*xk;
 
-C(1,1) = sin(phik+gamma)/((rp-rtol)*sin(gamma));
-C(1,2) = -cos(phik+gamma)/((rp-rtol)*sin(gamma));
-C(2,1) = -sin(phik-gamma)/((rp-rtol)*sin(gamma));
-C(2,2) = cos(phik-gamma)/((rp-rtol)*sin(gamma));
-%C(1,10) = L2*xk+L1*yk;
-%C(2,10) = -L4*xk-L3*yk;
+C(1,1) = L1;
+C(1,2) = -L2;
+C(2,1) = -L3;
+C(2,2) = L4;
+C(1,6) = L2*xk+L1*yk;
+C(2,6) = -L4*xk-L3*yk;
 
