ad46936b31b32c0f231938b1560f1ac4e403b5f4
roshenam <roshenam@stanford.edu>
2016-06-29 20:10:27 -0700

    Modified to work with attitude control version of 2D slack mpc

--- Animate.m
+++ Animate.m
@@ -1,7 +1,8 @@
-function [] = Animate(init, params, xtot, animate, filename, rotate)
+function [] = Animate(init, params, xtot, animate, filename, rotate, attitude)
 % If animate is 1, a VideoWriter object will be created and a video will be
 % saved of the animation. If rotate is 1, it will plot the rotating cone
-% constraint. xtot must include phi data. 
+% constraint. xtot must include phi data. If attitude is 1 then xtot should
+% have orientation data in it. 
 if animate
 v = VideoWriter(filename);
 v.FrameRate = 6;
@@ -11,23 +12,26 @@ xtot = xtot';
 xrange = linspace(-4,10);
 rA = params.rp; rtol = params.rtol; rB = params.rs;
 gamma = params.gamma;
-phi = params.phi; x0 = xtot(1,1); y0 = xtot(1,2); theta0 = xtot(1,3);
+phi = params.phi; x0 = xtot(1,1); y0 = xtot(1,2); theta0 = xtot(1,7);
 h = figure('Units','Normalized','Position',[.2,.1,.6,.6]);
 hold all
-xpos = xtot(:,1); ypos = xtot(:,2); thetapos = xtot(:,3)+phi;
+xpos = xtot(:,1); ypos = xtot(:,2); thetapos = xtot(:,7);
 viscircles([0,0],rA,'color','k','linewidth',1);
 dim = max([max(xtot(:,1)),max(xtot(:,2))]);
 xlim([-4,dim+1])
 ylim([-4,dim+1])
 axis('square')
-viscircles([rA*cos(phi), rA*sin(phi)],rA/10,'color','k');
+port = rectangle('Position',[rA*cos(phi)-rA/2,rA*sin(phi)-rA/2, rA, rA],'Curvature',[1,1],'facecolor','r');
 y1 = (sin(phi+gamma).*xrange./((rA-rtol)*sin(gamma))-1)./(cos(phi+gamma)/((rA-rtol)*sin(gamma)));
 y2 = (1+ sin(phi-gamma).*xrange./((rA-rtol)*sin(gamma)))./(cos(phi-gamma)/((rA-rtol)*sin(gamma)));
 cone1 = line(xrange,y1,'color','k','linewidth',2);
 cone2 = line(xrange,y2,'color','k','linewidth',2);
 
 freeflyer = rectangle('Position',[x0-rB, y0-rB, rB*2, rB*2],'Curvature',[1,1],'facecolor','g');
-%gripper = rectangle('Position',[x0-rB*cos(theta0)-rB/7, y0-rB*sin(theta0)-rB/7,2*rB/7,2*rB/7],'Curvature',[1,1],'facecolor','k');
+if attitude
+gripper = rectangle('Position',[x0-rB*cos(theta0)-rB/4, y0-rB*sin(theta0)-rB/4,2*rB/4,2*rB/4],'Curvature',[1,1],'facecolor','r');
+else
+end
 path = line(xtot(1,1),xtot(1,2),'color','b');
 
 frame = getframe;
@@ -36,12 +40,16 @@ writeVideo(v,frame);
 end
 for i=2:length(xtot)
     set(freeflyer, 'Position', [xpos(i)-rB, ypos(i)-rB, rB*2, rB*2]);
-    %set(gripper, 'Position', [xpos(i)-rB*cos(thetapos(i))-rB/7, ypos(i)-rB*sin(thetapos(i))-rB/7,2*rB/7,2*rB/7]);
+    if attitude
+    set(gripper, 'Position', [xpos(i)-rB*cos(thetapos(i))-rB/4, ypos(i)-rB*sin(thetapos(i))-rB/4,2*rB/4,2*rB/4]);
+    else
+    end
     set(path,'XData',xtot(1:i,1),'YData',xtot(1:i,2))
     if rotate
-        set(cone1,'YData',(sin(xtot(i,end)+gamma).*xrange./((rA-rtol)*sin(gamma))-1)./(cos(xtot(i,end)+gamma)/((rA-rtol)*sin(gamma))));
-        set(cone2,'Ydata',(1+ sin(xtot(i,end)-gamma).*xrange./((rA-rtol)*sin(gamma)))./(cos(xtot(i,end)-gamma)/((rA-rtol)*sin(gamma))));
+        set(cone1,'YData',(sin(xtot(i,6)+gamma).*xrange./((rA-rtol)*sin(gamma))-1)./(cos(xtot(i,6)+gamma)/((rA-rtol)*sin(gamma))));
+        set(cone2,'Ydata',(1+ sin(xtot(i,6)-gamma).*xrange./((rA-rtol)*sin(gamma)))./(cos(xtot(i,6)-gamma)/((rA-rtol)*sin(gamma))));
     end
+    set(port,'Position',[rA*cos(xtot(i,6))-rA/2, rA*sin(xtot(i,6))-rA/2,rA,rA])
     drawnow  
     pause(.1)
     if animate
