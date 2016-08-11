ccea0b5f60c36ec9aa0b184ade99aec3c51e6b2d
roshenam <roshenam@stanford.edu>
2016-08-11 13:30:37 -0700

    Added capability to crudely compute terminal conditions

--- MPT3_Test.m
+++ MPT3_Test.m
@@ -1,8 +1,8 @@
-% Defining parameters 
-phi = pi/4; omega = 5*pi/180; 
+% Defining parameters
+phi = pi/4; omega = 2*pi/180;
 params.phi = phi; % Phi is the angle of the port from the defined horizontal
 params.omega = omega; % Omega is the rotation in rad/sec of the target
-params.rp = .2; params.rtol = .2+.01;  params.rs = .2; params.gamma = pi/20;
+params.rp = .2; params.rtol = .2+.01;  params.rs = .2; params.gamma = 20*pi/180;
 % rp is the radius of the target, rs is the radius of the spacecraft, gamma
 % is the half angle of the constraint cone
 params.Umax = .5;
@@ -21,15 +21,15 @@ params.tantol = .4;
 % Constants defining the shape of the bounding function for normal
 % component of hte velocity
 
-rp = params.rp; rs = params.rs; 
-Ts = params.Ts; 
+rp = params.rp; rs = params.rs;
+Ts = params.Ts;
 betaHIGH = params.betaHIGH; betaLOW = params.betaLOW;
 eta = params.eta; tantol = params.tantol;
 gamma = params.gamma;
 
 % Creating dynamic system
 A = zeros(6,6); A(1,4) = 1; A(2,5) = 1; A(3,6) = 1;
-A(4,1) = omega^2; A(4,5) = 2*omega; 
+A(4,1) = omega^2; A(4,5) = 2*omega;
 A(5,2) = omega^2; A(5,4) = -2*omega;
 B = zeros(6,3); B(4,1) = 1; B(5,2) = 1; B(6,3) = 1;
 C = zeros(6,6);  D = zeros(6,3);
@@ -41,13 +41,13 @@ Ad = sysD.a; Bd = sysD.b; % Matrices for discrete system
 % Y is a vector of constrained outputs. C and D are matrices defining Y's
 % dependence on the states and control inputs
 
-Dd = zeros(5,3); 
+Dd = zeros(5,3);
 %Dd = zeros(3,3);
 Cd = zeros(5,6);
 %Cd = zeros(2,6);
 Cd(1,1) = -tan(gamma); Cd(1,2) = 1;
 Cd(2,1) = -tan(gamma); Cd(2,2) = -1;
-Cd(3,1) = -params.eta; Cd(3,2) = -params.eta; Cd(3,4) = -1; 
+Cd(3,1) = -params.eta; Cd(3,2) = -params.eta; Cd(3,4) = -1;
 Cd(4,1) = -params.eta; Cd(4,2) = -params.eta; Cd(4,5) = 1;
 Cd(5,1) = -params.eta; Cd(5,2) = -params.eta; Cd(5,5) = -1;
 
@@ -65,7 +65,7 @@ Ymax = [0; 0; -betaHIGH-eta*(rp+rs); tantol-eta*(rp+rs); tantol-eta*(rp+rs)];
 %Ymax = [0; 0; -betaHIGH-eta*(rp+rs)];
 
 % Creating weighting matrices for cost function
-Q = params.Qval.*eye(6); Q(4:6,4:6) = .5*params.Qval.*eye(3); R = params.Rval.*eye(3); 
+Q = params.Qval.*eye(6); Q(4:6,4:6) = .5*params.Qval.*eye(3); R = params.Rval.*eye(3);
 system = LTISystem('A',Ad,'B',Bd,'C',Cd,'D',Dd,'Ts',Ts);
 system.u.min = [-Umax; -Umax; -Tmax];
 system.u.max = [Umax; Umax; Tmax];
@@ -81,9 +81,10 @@ system.x.with('terminalSet');
 system.x.terminalSet = Tset;
 %%
 clc
+tic
 params.N = 30;
-N = params.N; 
-Nsim = 60;
+N = params.N;
+Nsim = 30;
 params.Nsim = Nsim;
 mpc = MPCController(system,N);
 %exp = mpc.toExplicit();
@@ -92,7 +93,7 @@ loop = ClosedLoop(mpc, system);
 % Specify initial conditions in frame with x axis intersecting with target
 % point. Then rotate into shifted frame with x axis along bottom edge of
 % cone
-x0 =  5; y0 = 0; theta0 = -pi/4; vx0 = -.5; vy0 = .5; thetadot0 = 5*pi/180;
+x0 =  5; y0 = 0; theta0 = -pi/4; vx0 = -.9; vy0 = .9; thetadot0 = 5*pi/180;
 Rmat = [cos(phi) -sin(phi); sin(phi) cos(phi)];
 rI = Rmat*[x0;y0];
 vI = Rmat*[vx0;vy0];
@@ -103,8 +104,8 @@ init = [rI(1) rI(2) theta0 vI(1) vI(2) thetadot0];
 nu0 = theta0-phi; nu0dot = thetadot0-omega;
 %x0vec = [r0(1); r0(2); nu0; v0(1); v0(2); nu0dot];
 x0vec = [x0; y0; nu0; vx0; vy0; nu0dot];
-data = loop.simulate(x0vec(1:6), Nsim)
-
+data = loop.simulate(x0vec(1:6), Nsim);
+toc
 xtotnonI = data.X; % Save data in non-inertial frame
 utotnonI = data.U; % Save thrust data in non-inertial frame
 vnbound = -eta.*data.X(1,:)-eta.*data.X(2,:)+betaHIGH+eta*(rp+rs);
@@ -112,6 +113,7 @@ vtbound = -eta.*data.X(1,:)-eta.*data.X(2,:)-tantol+eta*(rp+rs);
 xtot = zeros(8,Nsim+1);
 utot = zeros(3,Nsim);
 mult = 0:Nsim;
+time = Ts.*mult;
 xtot(8,:) = phi+Ts*omega.*mult;
 xtot(1,:) = xtotnonI(1,:).*cos(xtot(8,:))-xtotnonI(2,:).*sin(xtot(8,:));
 xtot(2,:) = xtotnonI(1,:).*sin(xtot(8,:))+xtotnonI(2,:).*cos(xtot(8,:));
@@ -125,14 +127,21 @@ utot(2,:) = utotnonI(1,:).*sin(xtot(8,1:end-1))+utotnonI(2,:).*cos(xtot(8,1:end-
 utot(3,:) = utotnonI(3,:);
 
 Animate(init,params,xtot,xtot(end,:),0,'hi',1,1)
-%%
-dist = sqrt(data.X(1,:).^2+data.X(2,:).^2);
+%% Determining incoming conditions upon impact
+
+d_grip_offset = 0.156;                              % [m] Distance between gripper CG and target CG when captured
+d_grippad_offset = 0.041;                           % [m] Distance between gripper CG and gripper pad
+l_finger = 0.13;                                    % Length of gripper finger
+
 idx = find(dist<=(rp+rs),1);
-final.vn = data.X(4,idx);
-final.vt = data.X(5,idx);
-final.vmag = norm([data.X(4,idx) data.X(5,idx)]);
-final.nu = data.X(3,idx)*180/pi;
-final.angattack = atan2(data.X(5,idx),data.X(4,idx));
-final.nudot = data.X(6,idx)*180/pi;
+final.vn = data.X(4,idx-1);
+final.vt = data.X(5,idx-1);
+final.vmag = norm([data.X(4,idx-1) data.X(5,idx-1)]);
+final.nu = data.X(3,idx-1)*180/pi;
+final.angattack = atan2(data.X(5,idx-1),data.X(4,idx-1));
+final.nudot = data.X(6,idx-1)*180/pi;
 final.thrust = sum(sqrt(utot(1,:).^2+utot(2,:).^2));
+d_rem = dist(idx-1) - (rp+rs);
+t_rem = d_rem/final.vmag;
+
 final
