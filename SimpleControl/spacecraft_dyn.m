0d9e21967608266c3cf877bbb7611bce4d94c025
roshenam <roshenam@stanford.edu>
2017-03-30 16:04:41 -0700

    Added ability to simulate noise and friction

--- SimpleControl/spacecraft_dyn.m
+++ SimpleControl/spacecraft_dyn.m
@@ -4,10 +4,37 @@ idx = find(P.t>=t,1);
 if isempty(idx)
     idx = length(P.t);
 end
-u = P.u(idx,:);
-u=u';
+unom = P.u(idx,:);
+unom=unom';
+y_des = P.state_des(idx,:);
+%y_noise = randn(4,1)./10;
+u_fric = -.01.*[y(3); y(4)]./sqrt(y(3)^2+y(4)^2);
+uLQR = -P.K*(y-y_des');
+utot = unom+uLQR;
+if utot(1) > P.umax
+    utot(1) = P.umax;
+elseif utot(1) < -P.umax
+    utot(1) = -P.umax;
+end
+
+if utot(2) > P.umax
+    utot(2) = P.umax;
+elseif utot(2) < -P.umax
+    utot(2) = -P.umax;
+end
+
+%figure(20)
+%plot(idx,uLQR(1),'ro')
+%hold all
+%plot(idx,uLQR(2),'go')
+%u = utot+u_fric;
+%u = unom+u_fric;
+%u = unom+u_fric;
+%u = unom;
+u = utot;
 %P.i = P.i+1;
 dydt = zeros(4,1);
+%xdot = P.A*(y+y_noise)+P.B*u;
 xdot = P.A*y+P.B*u;
 dydt(1) = xdot(1);
 dydt(2) = xdot(2);
