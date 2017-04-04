760cbbcf1cc20a391a3aa3c23d8ce95077200c3c
roshenam <roshenam@stanford.edu>
2017-04-04 10:15:32 -0700

    removed integral action

--- SimpleControl/SimLineLQR.m
+++ SimpleControl/SimLineLQR.m
@@ -1,4 +1,6 @@
-clear all; close all; clc;
+clear all; 
+%close all; 
+clc;
 addpath(genpath('~/Software/tbxmanager'))
 warning('off','MATLAB:nargchk:deprecated')
 
@@ -19,43 +21,49 @@ Pstruct.vyd = .03;
 thetad = pi/4;
 x0 = .1;
 y0 = 0;
-y0 = [x0, y0, 0, 0, pi, 0, x0, x0]';
+theta0 = 0;
+y0 = [x0, y0, 0, 0, theta0, 0, x0, y0, theta0]';
 Pstruct.tdis0 = 2;
-Pstruct.tdisf = 2.5;
+Pstruct.tdisf = 4;
 Pstruct.udis = 0;
+Pstruct.torquedis = 0;
 tspan = [0,7];
-Aaug = zeros(8,8); 
+Aaug = zeros(9,9); 
 Aaug(1:6,1:6) = A;
 Aaug(7,1) = 1;
 Aaug(8,2) = 1;
-Baug = zeros(8,3);
+Aaug(9,5) = 1;
+Baug = zeros(9,3);
 Baug(1:6,:) = B;
 Pstruct.A = Aaug;
 Pstruct.B = Baug;
 Pstruct.K = K;
 Pstruct.umax = .4/18;
 Pstruct.tmax = .3;
-Pstruct.R = zeros(8,6);
+Pstruct.R = zeros(9,6);
 Pstruct.R(7,1) = -1;
 Pstruct.R(8,2) = -1;
-Pstruct.Ki = [.1 0; .1 0; 0 0];
+Pstruct.R(9,5) = -1;
+Pstruct.Ki = [0 0 0; 0 0 0; 0 0 0];
 [tout,yout] = ode45(@(t,x) spacecraft_dynfull(t,x,Pstruct),tspan,y0);
 
-% figure
-% plot(yout(:,1),yout(:,2))
-% hold all
-% plot(0:3,0:3)
-% xlim([0,max(yout(:,1))])
-% ylim([0,max(yout(:,2))])
-% figure
-% plot(tout,yout(:,3))
-% hold all
-% plot(tout,yout(:,5))
+figure(1)
+hold all
+plot(yout(:,1),yout(:,2))
+plot(0:3,0:3)
+xlim([0,max(yout(:,1))])
+ylim([0,max(yout(:,2))])
+figure
+plot(yout(:,3))
+hold all
+plot(yout(:,4))
+figure
+plot(yout(:,1)-Pstruct.vxd.*tout)
 
-Ktot = zeros(3,8);
-Ktot(:,1:6) = K;
-Ktot(:,7:8) = Pstruct.Ki;
+%Ktot = zeros(3,8);
+%Ktot(:,1:6) = K;
+%Ktot(:,7:9) = Pstruct.Ki;
 
 folder_name = 'Line_filegen/';
 fname_dat = 'Line_data.dat';
-writeLineDat([folder_name fname_dat], Aaug, Baug, Ktot, Pstruct.R)
+writeLineDat([folder_name fname_dat], K)
--- SimpleControl/spacecraft_dynfull.m
+++ SimpleControl/spacecraft_dynfull.m
@@ -3,10 +3,12 @@ function dydt = spacecraft_dynfull(t,y,P)
 y_des = [P.vxd*t; P.vyd*t; P.vxd; P.vyd; 0; 0];
 if (P.tdis0<t && t<P.tdisf)
     u_dis = P.udis;
+    t_dis = P.torquedis;
 else
     u_dis = 0;
+    t_dis = 0;
 end
-u = -P.K*(y(1:6)-y_des)-P.Ki*y(7:8);
+u = -P.K*(y(1:6)-y_des)-P.Ki*y(7:9);
 if u(1) > P.umax
     u(1) = P.umax;
 elseif u(1) < -P.umax
@@ -17,13 +19,19 @@ if u(2) > P.umax
 elseif u(2) < -P.umax
     u(2) = -P.umax;
 end
+if u(3) > P.tmax
+    u(3) = P.tmax;
+elseif u(3) < -P.tmax
+    u(3) = -P.tmax;
+end
 % figure(3)
 % hold all
 % plot(t,u(1),'ro')
 % hold all
 % plot(t,u(2),'go')
-u = u+[u_dis; 0; 0];
-dydt = zeros(8,1);
+% plot(t,u(3),'bo')
+u = u+[u_dis; 0; t_dis];
+dydt = zeros(9,1);
 xdot = P.A*y+P.B*u+P.R*y_des;
 dydt(1) = xdot(1);
 dydt(2) = xdot(2);
@@ -33,6 +41,7 @@ dydt(5) = xdot(5);
 dydt(6) = xdot(6);
 dydt(7) = xdot(7);
 dydt(8) = xdot(8);
+dydt(9) = xdot(9);
 
 
 
